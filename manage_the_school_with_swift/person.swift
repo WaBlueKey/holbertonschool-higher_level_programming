c2a713e7f9ec76c15fe239e3d0bfdeab54691e12
Zee Adams <WaBlueKey@users.noreply.github.com>
2016-05-24 21:20:47 -0400

    Completed task 2

--- manage_the_school_with_swift/person.swift
+++ manage_the_school_with_swift/person.swift
@@ -9,38 +9,66 @@
 
  */
 
- protocol Classify {
+protocol Classify {
 
-     func isStudent() -> Bool
+    func isStudent() -> Bool
  }
 
- class Person {
+enum Subject {
+    case Math
+    case English
+    case French
+    case History
+}
 
-     var first_name: String
-     var last_name: String
-     var age: Int
+class Person {
 
-     init(first_name: String, last_name: String, age: Int) {
+    var first_name: String
+    var last_name: String
+    var age: Int
 
-         self.first_name = first_name
-         self.last_name = last_name
-         self.age = age
-     }
+    init(first_name: String, last_name: String, age: Int) {
 
-     func fullName() -> String {
-         return first_name + " " + last_name
-     }
- }
- class Mentor: Person, Classify {
-     func isStudent() -> Bool {
-         return false
-     }
-
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
     }
 
- class Student: Person, Classify {
-     func isStudent() -> Bool {
-         return true
-     }
+    func fullName() -> String {
+        return first_name + " " + last_name
+    }
+}
+class Mentor: Person, Classify {
+    func isStudent() -> Bool {
+        return false
+    }
+    let subject: Subject
 
- }
+    init(first_name: String, last_name: String, age: Int, subject: Subject = Subject.Math) {
+        self.subject = subject
+        super.init(first_name: first_name, last_name: last_name, age: age)
+    }
+
+    func stringSubject() -> String {
+        if self.subject == Subject.Math {
+            return "Math"
+        }
+        if self.subject == Subject.English {
+            return "English"
+        }
+        if self.subject == Subject.French {
+            return "French"
+        }
+        if self.subject == Subject.History {
+            return "History"
+        }
+        return "String"
+    }
+}
+
+class Student: Person, Classify {
+    func isStudent() -> Bool {
+        return true
+    }
+
+}
