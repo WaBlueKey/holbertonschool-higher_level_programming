bd1e5b2912297c253900d04fc917f016d3f29de4
Zee Adams <WaBlueKey@users.noreply.github.com>
2016-06-07 21:32:33 -0400

    updated spacing

--- basic_sql_playground/5-main.sql
+++ basic_sql_playground/5-main.sql
@@ -3,6 +3,7 @@ SELECT DISTINCT last_name FROM Person
     JOIN TVShowPerson ON TVShowPerson.person_id = Person.id,
          TVShow ON TVShowPerson.tvshow_id = TVShow.id
     WHERE name = "Game of Thrones" ORDER BY last_name ASC;
+
 /* count the number of people under 30 */
 SELECT COUNT(age) FROM Person WHERE age > 30;
 
@@ -11,5 +12,6 @@ SELECT Person.id, first_name, last_name, age, color, name FROM Person
     JOIN TVShowPerson ON TVShowPerson.person_id = Person.id,
          TVShow ON TVShow.id = TVShowPerson.tvshow_id,
          EyesColor ON EyesColor.person_id = Person.id;
+
 /* show the sum of all the ages in the Person table. */
 SELECT SUM(age) FROM Person;
