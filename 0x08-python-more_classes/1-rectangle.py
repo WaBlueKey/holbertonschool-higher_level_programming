181dffd08a0ae6c118a9d0ffea92a4658a56a56b
Zee Adams <wabluekey@gmail.com>
2017-04-22 00:01:54 -0400

    completed task 2 and updated task 1

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -19,7 +19,7 @@ class Rectangle:
 
     @width.setter
     def width(self, value):
-        """check if width value is an integer and is positive"""
+        """check if width value is an integer and is positive."""
         if type(value) is not int:
             raise TypeError("width must be an integer")
         if value < 0:
@@ -32,7 +32,7 @@ class Rectangle:
 
     @height.setter
     def height(self, value):
-        """check if height is an integer and is positive"""
+        """check if height is an integer and is positive."""
         if type(value) is not int:
                 raise TypeError("height must be an integer")
         if value < 0:
---
+++ 0x08-python-more_classes/2-rectangle.py
@@ -0,0 +1,56 @@
+#!/usr/bin/python3
+"""
+This file contains a class that defines a rectangle.
+"""
+
+
+class Rectangle:
+    """
+    This class defines a rectangle.
+    """
+
+    w_int = "width must be an integer"
+    w_pos = "width must be >= 0"
+    h_int = "height must be an integer"
+    h_pos = "height must be >= 0"
+
+    def __init__(self, width=0, height=0):
+        '''Initialize rectangle with width and height of 0.'''
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """check if width value is an integer and is positive."""
+        if type(value) is not int:
+            raise TypeError(w_int)
+        elif value < 0:
+            raise ValueError(w_pos)
+        else:
+            self.__width = value
+
+    @property
+    def height(self):
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """check if height is an integer and is positive."""
+        if type(value) is not int:
+            raise TypeError(h_int)
+        elif value < 0:
+            raise ValueError(h_pos)
+        else:
+            self.__height = value
+
+    def area(self):
+        return self.__width * self.__height
+
+    def perimeter(self):
+        if self.__width == 0 or self.__height == 0:
+            return 0
+        return (self.__width * 2) + (self.__height * 2)
