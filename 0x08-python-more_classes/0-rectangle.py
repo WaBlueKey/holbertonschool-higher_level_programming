daac9f2c81dc3f276b114807f5d1360a299f2c9c
Zee Adams <wabluekey@gmail.com>
2017-04-21 23:55:25 -0400

    completed tasks 0 and 1

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""
+This file contains one class that defines a rectangle.
+"""
+
+
+class Rectangle:
+    pass
---
+++ 0x08-python-more_classes/1-rectangle.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+"""
+This file contains a class that defines a rectangle.
+"""
+
+
+class Rectangle:
+    """
+    This class defines a rectangle.
+    """
+
+    def __init__(self, width=0, height=0):
+        self.height = height
+        self.width = width
+
+    @property
+    def width(self):
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """check if width value is an integer and is positive"""
+        if type(value) is not int:
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """check if height is an integer and is positive"""
+        if type(value) is not int:
+                raise TypeError("height must be an integer")
+        if value < 0:
+                raise ValueError("height must be >= 0")
+        self.__height = value
